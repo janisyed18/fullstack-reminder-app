b65bb0778480fc454e94689e0d9a712f7a125248
Jani Syed <51842082+janisyed18@users.noreply.github.com>
2025-06-18 23:46:13 -0500

    Commit Changes

--- Reminder-App/src/main/java/com/reminderApp/Reminder/App/entity/Reminder.java
+++ Reminder-App/src/main/java/com/reminderApp/Reminder/App/entity/Reminder.java
@@ -9,7 +9,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "reminder")
+@Table(name = "reminder", schema = "reminderapp")
 @Data
 @NoArgsConstructor
 public class Reminder {
